8c0aff97da2ff19a23bbc37dbd055af8d1cf8481
skogard <skogard@protonmail.com>
2022-07-15 16:36:58 -0400

    add authentication

--- handlers/ui/main.js
+++ handlers/ui/main.js
@@ -2,12 +2,13 @@ const path = require('path')
 module.exports = (nuron) => {
   nuron.app.get("/", async (req, res) => {
     let key = nuron.core.wallet.key()
+    let adminEnabled = nuron.admins && nuron.admins.length > 0
     if (key) {
-      res.render("view/home", { name: nuron.core.wallet.name(), chainId: req.params.chainId })
+      res.render("view/home", { name: nuron.core.wallet.name(), chainId: req.params.chainId, adminEnabled })
     } else {
       let wallets = await nuron.core.wallet.get()
       console.log("Wallets", wallets)
-      res.render("wallet/loggedout", { wallets })
+      res.render("wallet/loggedout", { wallets, adminEnabled })
     }
   })
 }
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const Privateparty = require('privateparty')
 const ejs = require('ejs')
 const fs = require('fs')
 const mime = require('mime-types')
@@ -88,13 +89,53 @@ class Nuron {
     // Initialize App
     this.app = express()
     this.port = options && options.port ? options.port : 42000
-    this.app.use(cors(Config.cors))
-    this.app.options('*', cors(Config.cors))
+//    this.app.use(cors(Config.cors))
+//    this.app.options('*', cors(Config.cors))
     this.app.use(express.urlencoded({ extended: true }))
     this.app.use(express.json())
     this.app.set('view engine', 'ejs');
     this.app.set('views', path.join(__dirname, 'views'));
     this.app.use(express.static(path.join(__dirname, 'public')))
+
+    // Provide admin UI for admins
+    this.party = new Privateparty({
+      secret: options.secret,
+      app: this.app,
+      cors: options.cors
+    })
+
+
+    let protector = (options.beforeAuth ? options.beforeAuth(this.party) : {})
+    this.admins = options.admins
+
+//    let protector = {}
+//    if (options.admins) {
+//      this.admins = options.admins
+//      this.party.add("admin", {
+//        authorize: async (req, account) => {
+//          // only allow admins to use
+//          for(let admin of this.admins) {
+//            if (account === admin.toLowerCase()) {
+//              return { authorized: true }
+//            }
+//          }
+//          throw new Error("not an admin account")
+//        }
+//      })
+//      protector.cookie = "admin"
+//    }
+//
+//    // Token authentication
+//    if (options.tokens) {
+//      this.party.add("api", { tokens: options.tokens })
+//      protector.token = "api"
+//    }
+
+
+    // Protect
+    if (Object.keys(protector).length > 0) this.app.use(this.party.protect(protector))
+
+    if (options.afterAuth) options.afterAuth(this.party) 
 
     // API handlers
     Handlers.api.fs(this)
